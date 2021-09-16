46c4be330ead31b35a99750a1a7933ab5118f076
jineshnagori <jineshn21@gmail.com>
2021-09-16 09:07:10 +0530

    nav bar added in mobile view

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,15 +1,5 @@
 $(document).ready(function(){
 
-    //Toggle Nav for mobile  menu
-    $('.open_bar').click(function(){
-        $('nav').addClass('active-nav');
-        $('.menu-toggle').addClass('closee');
-    });
-    $('.close_bar').click(function(){
-        $('nav').removeClass('active-nav');
-        $('.menu-toggle').removeClass('closee');
-    });
-
    //Counter Up
    $('.counter').counterUp({
      time: 1500
